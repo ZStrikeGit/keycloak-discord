c145e10118c26fde877250699c9f89d5e8c99aaf
ZStrikeGit <85562552+ZStrikeGit@users.noreply.github.com>
2024-10-29 14:35:20 -0400

    Update DiscordIdentityProvider.java

--- src/main/java/org/keycloak/social/discord/DiscordIdentityProvider.java
+++ src/main/java/org/keycloak/social/discord/DiscordIdentityProvider.java
@@ -45,7 +45,7 @@ public class DiscordIdentityProvider extends AbstractOAuth2IdentityProvider<Disc
     public static final String TOKEN_URL = "https://discord.com/api/oauth2/token";
     public static final String PROFILE_URL = "https://discord.com/api/users/@me";
     public static final String GROUP_URL = "https://discord.com/api/users/@me/guilds";
-    public static final String DEFAULT_SCOPE = "identify email";
+    public static final String DEFAULT_SCOPE = "identify openid email";
     public static final String GUILDS_SCOPE = "guilds";
 
     public DiscordIdentityProvider(KeycloakSession session, DiscordIdentityProviderConfig config) {
